5277dc1f1ebf5f09544bca6e5bc40c8f7167fb25
yoavk-sudo <kenkendler123@gmail.com>
2023-07-16 23:50:38 +0300

    nhsdkjfghfjksd

--- Assets/Scripts/PlayerChaseAI.cs
+++ Assets/Scripts/PlayerChaseAI.cs
@@ -6,57 +6,63 @@ using UnityEngine;
 public class PlayerChaseAI : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] SpriteRenderer PlayerSprite;
     [SerializeField] Collider2D playerCollider;
     [SerializeField] Collider2D enemyCollider;
     [SerializeField] Animator attackAnimator;
     [SerializeField] float speed = 3f;
     [SerializeField] float maxDistance = 1f;
 
-    //[SerializeField] float attackCooldown = 0.5f;
+    public static float distance;
+    bool isInRange = false;
 
-    private static float distance;
-    private bool isInRange = false;
+    private void Start()
+    {
+        if (player.transform.position.x < enemyCollider.transform.position.x)
+        {
+            Debug.Log("true");
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
 
         distance = Vector2.Distance(player.transform.position, transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        if (distance < maxDistance) isInRange = true;
+        //Vector2 direction = player.transform.position - transform.position;
+        if (distance - 10 < maxDistance) isInRange = true;
         else isInRange = false;
     }
 
     [Obsolete]
     private void FixedUpdate()
-      {
-          if (FocusPlayer.Pause == false)
-          {
-              gameObject.SetActive(true);
+    {
+        if (FocusPlayer.Pause == false)
+        {
+            PlayerSprite.enabled = true;
             //if (EnemyScript.isDead) return;
             if (!isInRange) MoveTowardsPlayer();
-              if (isInRange) AttackPlayer();
-          }
-
-          else if(FocusPlayer.Pause == true)
-          {
-            gameObject.SetActive(false);
+            if (isInRange) AttackPlayer();
         }
+
+        else if (FocusPlayer.Pause == true)
+        {
+            PlayerSprite.enabled = false;
+        }
+
     }
 
     private void AttackPlayer()
     {
         // play animation
-        // if collides with player, player takes damage
-        // Update is called once per frame
+        attackAnimator.SetBool("Attack", true);
     }
 
     private void MoveTowardsPlayer()
     {
-        Vector2 targetPosition = new Vector2(player.transform.position.x - maxDistance, transform.position.y);
-        transform.position = new Vector2(Mathf.MoveTowards(transform.position.x, targetPosition.x, speed * Time.deltaTime), transform.position.y);
-        //Vector2 targetPosition = new Vector2(player.transform.position.x - maxDistance, transform.position.y);
-        //transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        Vector3 targetPosition = new Vector3(player.transform.position.x - maxDistance, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        attackAnimator.SetBool("Attack", false);
     }
 }
